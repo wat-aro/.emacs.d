98554def1ae325eaac6c867a2b3a77956d9f7f5f
wat-aro <kazutas1008@gmail.com>
2015-01-19 21:48:48 +0900

    Modify init.el about gauche.

--- init.el
+++ init.el
@@ -367,7 +367,7 @@
 ; --------------------------------------------------------------------
 (setq process-coding-system-alist
       (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
-(setq scheme-program-name "/usr/local/bin/gosh -i")
+(setq scheme-program-name "gosh -i")
 
 (autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
 (autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
