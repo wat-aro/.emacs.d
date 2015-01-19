0c55a38a25c84a394cfde68d7b1f4e3e9928c83f
wat-aro <kazutas1008@gmail.com>
2015-01-19 22:46:43 +0900

    goshの起動パスをシステムごとに分けた

--- init.el
+++ init.el
@@ -367,7 +367,10 @@
 ; --------------------------------------------------------------------
 (setq process-coding-system-alist
       (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
-(setq scheme-program-name "gosh -i")
+(when (eq system-type 'gnu/linux)
+  (setq scheme-program-name "gosh -i"))
+(when (eq system-type 'darwin)
+  (setq scheme-program-name "/usr/local/bin/gosh -i"))
 
 (autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
 (autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
