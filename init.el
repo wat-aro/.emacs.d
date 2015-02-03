a16f92a7c9acbf998877c11b3a58a864ba63e8d5
wat-aro <kazutas1008@gmail.com>
2015-02-03 23:36:02 +0900

    add dired setting

--- init.el
+++ init.el
@@ -98,6 +98,14 @@
 (add-to-list 'ac-modes 'web-mode)
 
 
+;; dired
+;; diredを2つのウィンドウで開いている時に、デフォルトの移動orコピー先をもう一方のdiredで開いているディレクトリにする
+(setq dired-dwim-target t)
+;; ディレクトリを再帰的にコピーする
+(setq dired-recursive-copies 'always)
+;; diredバッファでC-sした時にファイル名だけにマッチするように
+(setq dired-isearch-filenames t)
+
 ; ======================================================================
 ;       open-junk-file
 ; ======================================================================
