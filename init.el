6c3822b58cbd7683db3b0aa8687e4436a25ed110
wat-aro <kazutas1008@gmail.com>
2015-05-31 09:11:46 +0900

    add js2-mode

--- init.el
+++ init.el
@@ -851,3 +851,9 @@
   ("blade"  . "\\.blade\\.")))
 
 ; ======================================================================
+
+
+;; js2-mode
+(require 'js2-mode)
+(autoload 'js2-mode "js" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
