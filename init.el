66b5550da933e4a8c19c0fd6a7ff043a0aa3736e
wat-aro <kazutas1008@gmail.com>
2015-06-27 14:42:10 +0900

    xmpfilter関連の修正

--- init.el
+++ init.el
@@ -1,6 +1,11 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 ;; ------------------------------------------------------------------------
 
+;; load environment value
+(load-file (expand-file-name "~/.emacs.d/shellenv.el"))
+(dolist (path (reverse (split-string (getenv "PATH") ":")))
+  (add-to-list 'exec-path path))
+
 ;; @ load-path
 (setq default-directory "~/")
 (setq command-line-default-directory "~/")
