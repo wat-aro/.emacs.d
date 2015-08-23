862d864cd46eb5b1b72b1e0cf6c76d846ef94890
wat-aro <kazutas1008@gmail.com>
2015-08-23 11:00:50 +0900

    透明度を変更

--- init.el
+++ init.el
@@ -474,7 +474,7 @@
 (setq comint-scroll-show-maximum-output t) ;; shell-mode
 
 ;; フレームの透明度
-(set-frame-parameter (selected-frame) 'alpha '(0.70))
+(set-frame-parameter (selected-frame) 'alpha '(0.85))
 
 
 ;; モードラインに行番号表示
